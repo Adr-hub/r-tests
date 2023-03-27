d8136ef36b0826f00d5bd88e86b4b2daffe47ef2
Adrien <103190086+Adr-hub@users.noreply.github.com>
2023-03-27 20:00:57 +0200

    Test commit

--- routing-tests/src/pages/testFile1.js
+++ routing-tests/src/pages/testFile1.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import '../styles/fr.scss';
 
 const TestFile1 = () => {
-    let mediaTest = window.matchMedia("(hover: none)");
+    const [media, findMedia] = useState(undefined);
+    useEffect(() => {
+        let mediaTest = window.matchMedia("(hover: none)");
+
+        if (mediaTest.matches) {
+            findMedia(true);
+        }
+
+    }, []);
+
     return <>
         <Link className="frLinks" to={'/test'}>parcours</Link>
-        <Link className="frLinks" to={mediaTest.matches ? '/error' : '/en/education'}>education</Link>
+        <Link className="frLinks" to={media ? '/error' : '/en/education'}>education</Link>
         <p style={{ visibility: "hidden" }}>test</p>
         <p style={{ visibility: "hidden" }}>test</p>
         <p style={{ visibility: "hidden" }}>test</p>
