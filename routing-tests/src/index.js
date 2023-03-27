f0d2c002b11760705fd4e3d4df05c7698521ed86
Adrien <103190086+Adr-hub@users.noreply.github.com>
2023-03-27 13:35:58 +0200

    Test commit

--- routing-tests/src/index.js
+++ routing-tests/src/index.js
@@ -17,28 +17,30 @@ const frontRoutes = createBrowserRouter([
   },
   {
     path: '/test',
-    element: <TestFile1 />
-  },
-  {
-    path: '/parcours',
-    element: <TestFile2 />
-  },
-  {
-    path: '/projets',
-    element: <TestFile3 />
-  },
-  {
-    path: '/en/portfolio',
-    element: <TestFile4 />
-  },
-  {
-    path: '/en/education',
-    element: <TestFile5 />
-  },
-  {
-    path: '/en/projects',
-    element: <TestFile6 />
-  },
+    element: <TestFile1 />,
+    children: [
+      {
+        path: '/parcours',
+        element: <TestFile2 />
+      },
+      {
+        path: '/projets',
+        element: <TestFile3 />
+      },
+      {
+        path: '/en/portfolio',
+        element: <TestFile4 />
+      },
+      {
+        path: '/en/education',
+        element: <TestFile5 />
+      },
+      {
+        path: '/en/projects',
+        element: <TestFile6 />
+      }
+    ]
+  }
 
 ]);
 
