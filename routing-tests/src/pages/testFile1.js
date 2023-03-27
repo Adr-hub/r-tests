5b70b1de123b86ac243d855cbe21e8db01f89af1
Adrien <103190086+Adr-hub@users.noreply.github.com>
2023-03-27 12:58:52 +0200

    Test commit

--- routing-tests/src/pages/testFile1.js
+++ routing-tests/src/pages/testFile1.js
@@ -4,7 +4,46 @@ import '../styles/fr.scss';
 const TestFile1 = () => {
     return <>
         <Link className="frLinks" to={'/test'}>parcours</Link>
-        <Link className="frLinks" to={'/en/education'}>education</Link>
+        <Link className="frLinks" to={'#test'}>education</Link>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p hidden>test</p>
+        <p id="test">test</p>
     </>
 }
 export default TestFile1;
